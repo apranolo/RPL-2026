cf3f68ffb6bb43e8edd6f107aa0f35acea63de98
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-04-02 09:18:16 +0700

    Add console command to refresh featured journals cache and create HomeController for welcome page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\PublicHomeService;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class HomeController extends Controller
+{
+    public function __construct(
+        protected PublicHomeService $homeService
+    ) {}
+
+    /**
+     * Display the welcome page.
+     */
+    public function index(Request $request): Response
+    {
+        $featuredJournals = $this->homeService->getFeaturedJournals();
+        $sintaStats = $this->homeService->getSintaStats();
+        $overallStats = $this->homeService->getOverallStats();
+
+        return Inertia::render('welcome', [
+            'featuredJournals' => $featuredJournals,
+            'sintaStats' => $sintaStats,
+            'totalUniversities' => $overallStats['totalUniversities'],
+            'totalJournals' => $overallStats['totalJournals'],
+        ]);
+    }
+}
