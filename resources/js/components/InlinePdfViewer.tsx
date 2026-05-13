de61518ba356051aa57c4446096c243d1c347119
BeeBeecuy <habibiirfan2nd@gmail.com>
2026-05-14 05:19:21 +0700

    fix InlinePdfViewer

--- resources/js/components/InlinePdfViewer.tsx
+++ resources/js/components/InlinePdfViewer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface Props {
     fileUrl: string;
     title?: string;
@@ -7,13 +5,8 @@ interface Props {
 
 export default function InlinePdfViewer({ fileUrl, title = 'Dokumen Naskah' }: Props) {
     return (
-        <div className="w-full h-[600px] border border-gray-300 rounded-md overflow-hidden">
-            <iframe 
-                src={fileUrl} 
-                title={title}
-                className="w-full h-full"
-                style={{ border: 'none' }}
-            />
+        <div className="h-[600px] w-full overflow-hidden rounded-md border border-gray-300">
+            <iframe src={fileUrl} title={title} className="h-full w-full" style={{ border: 'none' }} />
         </div>
     );
 }
