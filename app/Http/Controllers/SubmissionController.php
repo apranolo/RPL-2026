40108b4a85faeb70d4752f45345d4acc995be935
DzakyRidhwan <dzakyridhwan@gmail.com>
2026-05-13 20:11:08 +0700

    feat: add SubmissionController

---
+++ app/Http/Controllers/SubmissionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Submission;
+use Inertia\Inertia;
+
+class SubmissionController extends Controller
+{
+    public function show($id)
+    {
+        $submission = Submission::with([
+            'author',
+            'statusHistories',
+            'reviewer'
+        ])->findOrFail($id);
+
+        // memastikan author hanya bisa melihat submission miliknya
+        if ($submission->author_id !== auth()->id()) {
+            abort(403);
+        }
+
+        return Inertia::render('Author/Submissions/Show', [
+            'submission' => $submission,
+            'tracking' => $submission->statusHistories
+        ]);
+    }
+}
