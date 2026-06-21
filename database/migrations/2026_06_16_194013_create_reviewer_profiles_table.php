8caa9caed6e31d6ae268b430256695b5efeade6f
aditiyasubakti226 <155351822+aditiyasubakti226@users.noreply.github.com>
2026-06-21 22:11:16 +0700

    Update 2026_06_16_194013_create_reviewer_profiles_table.php
    udah di perbaiki agar sesuai

--- database/migrations/2026_06_16_194013_create_reviewer_profiles_table.php
+++ database/migrations/2026_06_16_194013_create_reviewer_profiles_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('reviewer_profiles', function (Blueprint $table) {
@@ -13,10 +14,16 @@ return new class extends Migration {
             $table->foreignId('user_id')
                 ->constrained('users')
                 ->cascadeOnDelete();
-            $table->json('skills')->nullable();
-            $table->unsignedInteger('total_reviews')->default(0);
-            $table->unsignedInteger('completed_reviews')->default(0);
-            $table->text('bio')->nullable();
+
+            $table->json('research_interests')->nullable();
+
+            $table->unsignedInteger('total_reviews')
+                ->default(0);
+
+            $table->unsignedInteger('completed_reviews')
+                ->default(0);
+
+            $table->text('biography')->nullable();
 
             $table->timestamps();
 
