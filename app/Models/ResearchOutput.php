8274bb362bbfd75d03dba7214eee62a766808ba7
Hidayat Lossen <dayatlossentuasikal@gmail.com>
2026-07-16 11:26:03 +0700

    feat(output): add missing columns for research and book outputs

--- app/Models/ResearchOutput.php
+++ app/Models/ResearchOutput.php
@@ -48,9 +48,12 @@ class ResearchOutput extends Model
         return $this->jenis_luaran === 'Buku' && $this->outputable ? $this->outputable->isbn : null;
     }
 
-    public function getTautanPublikasiAttribute()
+    public function getTautanPublikasiAttribute($value)
     {
-        return $this->jenis_luaran === 'Jurnal' && $this->outputable ? $this->outputable->url : null;
+        if ($this->jenis_luaran === 'Jurnal' && $this->outputable) {
+            return $this->outputable->url ?? $value;
+        }
+        return $value;
     }
 
     protected $fillable = [
@@ -59,9 +62,11 @@ class ResearchOutput extends Model
         'jenis_luaran',
         'judul_luaran',
         'tahun_capaian',
+        'penulis_atau_pencipta',
         'file_sertifikat_atau_cover',
         'status_verifikasi',
         'keterangan',
+        'tautan_publikasi',
         'outputable_type',
         'outputable_id',
     ];
